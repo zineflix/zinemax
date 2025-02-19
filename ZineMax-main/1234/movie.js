9d9e86d73aca6fddb068b9d25e777cb4c2f043a8
zineflix <zineflix0@gmail.com>
2025-02-19 17:28:27 +0800

    Update movie.js

--- ZineMax-main/1234/movie.js
+++ ZineMax-main/1234/movie.js
@@ -1,4 +1,4 @@
-const apiKey = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDB API Key
+    const apiKey = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDB API Key
 const baseUrl = 'https://api.themoviedb.org/3';
 
 // Helper function to fetch movies and populate the row
